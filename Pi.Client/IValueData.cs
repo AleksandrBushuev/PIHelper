f15fb06d7255f0a4b7e30153cfcca2abbe263d63
Aleksandr Bushuev <aleksandr.bushuev@indusoft.ru>
2021-10-28 10:46:38 +0500

    Добавил клиента AFSDK

---
+++ Pi.Client/IValueData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pi.Client
+{
+    public interface IValueData
+    {
+        object GetValue();
+
+        DateTime GetTimestamp();
+    }
+}
